11eb9333abac625056cc22bfed9274fcee5a5a1c
Megan Shepherd <mshepherd@bamboohr.com>
2019-10-15 21:51:21 -0600

    getting tailwind set up, config colors, etc, what a rollercoaster of fun

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        tan: {
+          lighter: '#F9F7F4',
+          darker: '#F5F1EA'
+        }
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
---
+++ tailwind.js
@@ -0,0 +1,21 @@
+module.exports = {
+  theme: {
+    spacing: {
+      '3': '13px'
+    },
+    extend: {
+      colors: {
+        tan: {
+          lighter: '#F9F7F4',
+          darker: '#F5F1EA',
+          darkest: '#DBD9D6'
+        },
+        brown: {
+          dark: '#372113'
+        }
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
